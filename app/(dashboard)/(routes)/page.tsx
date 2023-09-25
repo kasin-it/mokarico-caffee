c6530a7a02290d7108bdadc959276aba4108de96
KACPER SINILO <kacperosinilo@gmail.com>
2023-09-25 20:44:16 +0200

    buttons work

--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -26,28 +26,25 @@ function Home() {
   const navbarProperties = useNavbarProperties();
   const arrowUp = useArrowUpModal();
 
-  
   useEffect(() => {
     // Function to handle scroll events
     const handleScroll = () => {
       const screanHeight = window.scrollY;
-      
-      const isTop = screanHeight === 0
-      
+
+      const isTop = screanHeight === 0;
+
       if (isTop) {
         arrowUp.onClose();
-        
-        
       } else if (!isTop) {
         arrowUp.onOpen();
       }
-      
+
       navbarProperties.setisTransparent(
-        isTop && !(loginModal.isOpen || searchModal.isOpen)
-        );
-      };
-      
-      // Manually trigger the scroll event once after the component is mounted
+        isTop && !(loginModal.isOpen || searchModal.isOpen),
+      );
+    };
+
+    // Manually trigger the scroll event once after the component is mounted
     handleScroll();
 
     // Attach the scroll event listener
@@ -61,24 +58,24 @@ function Home() {
 
   return (
     <>
-      <section className='z-50 flex h-full w-full items-center justify-center'>
-        <div className='absolute left-0 hidden h-full w-[50%] md:block'></div>
-        <div className='absolute right-0 h-full w-full bg-blue-200 md:w-[50%]'>
+      <section className="z-50 flex h-full w-full items-center justify-center">
+        <div className="absolute left-0 hidden h-full w-[50%] md:block"></div>
+        <div className="absolute right-0 h-full w-full bg-blue-200 md:w-[50%]">
           <video
-            className='h-full w-full object-cover opacity-80'
+            className="h-full w-full object-cover opacity-80"
             autoPlay
             loop
             muted
             playsInline
           >
             {/* Use the appropriate URL to the video file */}
-            <source src='/video/beans.mp4' type='video/mp4' />
+            <source src="/video/beans.mp4" type="video/mp4" />
             {/* Add more <source> elements for other video formats if necessary */}
             Your browser does not support the video tag.
           </video>
         </div>
 
-        <div className='absolute left-0'>
+        <div className="absolute left-0">
           <h1>
             The Stylish
             <br /> coffee
@@ -90,42 +87,42 @@ function Home() {
           </p>
           <AnimatedButton
             secoundChildren={
-              <ArrowDownCircleIcon className='rotate-[-90deg] text-white' />
+              <ArrowDownCircleIcon className="rotate-[-90deg] text-white" />
             }
-            className='rotate-90'
+            className="rotate-90"
             width={140}
             // height={64} NOT working for now
           >
             BROWSE
           </AnimatedButton>
         </div>
-        <div className='z-20 flex'>
+        <div className="z-20 flex">
           <Image
             src={hero_img}
-            alt='hero'
+            alt="hero"
             width={hero_img.width}
             height={hero_img.height}
           />
         </div>
-        <div className='absolute right-0'>
-          <SocialmediaProviders className='text-white' />
+        <div className="absolute right-0">
+          <SocialmediaProviders className="text-white" />
         </div>
       </section>
-      <section className='relative mt-11 flex justify-center'>
+      <section className="relative mt-11 flex justify-center">
         <Image
           src={story_img}
-          alt='story'
+          alt="story"
           width={story_img.width / 2}
           height={story_img.height / 2}
         />
-        <article className='flex flex-col'>
-          <h1 className='text-7xl'>
+        <article className="flex flex-col">
+          <h1 className="text-7xl">
             A century <br /> long story
           </h1>
           <p>
             Where tradition and quality maintain <br /> their values over time.
           </p>
-          <section className='grid grid-cols-2'>
+          <section className="grid grid-cols-2">
             {/* <article className="w-[200px]">
 						</article>
 						<article className="w-[200px]">
@@ -136,87 +133,103 @@ function Home() {
 					</article> */}
             <Link
               href={'/'}
-              className='group flex h-[200px] w-[200px] flex-col items-center justify-end pb-12'
+              className="group flex h-[200px] w-[200px] flex-col items-center justify-end pb-12"
             >
               <Image
                 src={origini_svg}
-                alt='origini'
-                className='group-hover:opacity-30'
+                alt="origini"
+                className="group-hover:opacity-30"
               />
               <AnimatedButton width={140}>COMPANY</AnimatedButton>
             </Link>
             <Link
               href={'/'}
-              className='group flex h-[200px] w-[200px] flex-col items-center justify-end border-b border-l pb-12'
+              className="group flex h-[200px] w-[200px] flex-col items-center justify-end border-b border-l pb-12"
             >
               <Image
                 src={miscele_svg}
-                alt='miscele'
-                className='group-hover:opacity-30'
+                alt="miscele"
+                className="group-hover:opacity-30"
               />
               <AnimatedButton width={140}>BLENDS</AnimatedButton>
             </Link>
             <Link
               href={'/'}
-              className='group flex h-[200px] w-[200px] flex-col items-center justify-end border-r border-t pb-12'
+              className="group flex h-[200px] w-[200px] flex-col items-center justify-end border-r border-t pb-12"
             >
               <Image
                 src={qualita_svg}
-                alt='qualita'
-                className='group-hover:opacity-30'
+                alt="qualita"
+                className="group-hover:opacity-30"
               />
               <AnimatedButton width={140}>QUALITY</AnimatedButton>
             </Link>
             <Link
               href={'/'}
-              className='group flex h-[200px] w-[200px] flex-col items-center justify-end pb-12'
+              className="group flex h-[200px] w-[200px] flex-col items-center justify-end pb-12"
             >
               <Image
                 src={bioworld_svg}
-                alt='bioworld'
-                className='group-hover:opacity-30'
+                alt="bioworld"
+                className="group-hover:opacity-30"
               />
               <AnimatedButton width={140}>BIO WORLD</AnimatedButton>
             </Link>
           </section>
-          <span className='absolute right-[80px] top-[370px] rotate-90'>
+          <span className="absolute right-[80px] top-[370px] rotate-90">
             1936-2018
           </span>
         </article>
       </section>
-      <section className='flex w-full items-center justify-center pb-28 pt-20'>
-        <article className='relative top-[100px] mr-[200px]'>
+      <section className="w-full items-center justify-center pb-28 pt-20 hidden lg:flex px-10">
+        <article className="relative top-[100px] mr-[200px]">
           <Image
             src={cups_img}
-            alt='cups'
+            alt="cups"
             width={cups_img.width}
             height={cups_img.height}
           />
-          <div className='absolute right-[-100px] top-[50%]'>
+          <div className="absolute right-[-100px] top-[50%]">
             <h2>
               Four your <br /> Businness
             </h2>
-            <AnimatedButton childrenClassName='justify-end pe-10' width={200} className=''>
+            <AnimatedButton
+              childrenClassName="justify-end pe-10"
+              width={220}
+              className=""
+            >
               DISCOVER
             </AnimatedButton>
           </div>
         </article>
-        <article className='relative'>
+        <article className="relative">
           <Image
             src={home_office_img}
-            alt='cups'
+            alt="cups"
             width={home_office_img.width}
             height={home_office_img.height}
           />
-          <div className='absolute right-[-100px] top-[50%]'>
+          <div className="absolute right-[-100px] top-[50%]">
             <h2>
               Four your <br /> Businness
             </h2>
-            <AnimatedButton childrenClassName='justify-end pe-10' width={200}>
+            <AnimatedButton
+              childrenClassName="justify-end pe-10"
+              width={260}
+              // className="w-[260px]"
+            >
               DISCOVER
             </AnimatedButton>
           </div>
         </article>
+      </section>
+      <section className="w-full px-5 flex lg:hidden flex-col">
+        <Image
+          src={cups_img}
+          alt="cups"
+          width={cups_img.width}
+          height={cups_img.height}
+        />
       </section>
     </>
   );
--- components/footer.tsx
+++ components/footer.tsx
@@ -111,8 +111,8 @@ function Footer() {
   ];
 
   return (
-    <footer className='max-w-[1140px]'>
-      <Separator className='max-w-[1140px]' />
+    <footer className="max-w-[1140px]">
+      <Separator className="max-w-[1140px]" />
       <section className={'py-6'}>
         <h3>Certifications are an appropriate reward for our quality </h3>
         <article className={'flex space-x-5 px-11'}>
@@ -156,8 +156,12 @@ function Footer() {
           />
         </article>
       </section>
-      <Separator className='max-w-[1140px]' />
-      <section className={'flex flex-col lg:flex-row py-10 px-11 xxl:px-0 justify-center'}>
+      <Separator className="max-w-[1140px]" />
+      <section
+        className={
+          'flex flex-col lg:flex-row py-10 px-11 xxl:px-0 justify-center'
+        }
+      >
         <form className={'w-full lg:w-[600px]'}>
           Keep up to date with us
           <input
@@ -248,7 +252,7 @@ function Footer() {
           ))}
         </section>
       </section>
-      <Separator className='max-w-[1140px]' />
+      <Separator className="max-w-[1140px]" />
       <article>
         <Headphones />
         We are available to answer any query by phone from Mon - Fri 9:00 -12:30
