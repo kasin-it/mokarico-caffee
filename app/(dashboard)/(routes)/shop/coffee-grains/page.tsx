5a06cac5acc6114a46f3e2390bdc263953a224c9
KACPER SINILO <kacperosinilo@gmail.com>
2023-10-04 16:59:47 +0200

    adding coffee grains page

---
+++ app/(dashboard)/(routes)/shop/coffee-grains/page.tsx
@@ -0,0 +1,14 @@
+import Separator from '@/components/ui/separator';
+
+function CoffeeGrainsPage() {
+  return (
+    <section className="mt-[100px] flex items-center justify-center px-20  mb-10">
+      <section className="w-full space-y-3">
+        <h1 className="font-playfair-display text-7xl">Coffee Grains</h1>
+        <p>Home / Shop /Coffee Grains</p>
+      <Separator className='bg-gray-600' />
+      </section>
+    </section>
+  );
+}
+export default CoffeeGrainsPage;
--- app/(dashboard)/(routes)/shop/forms/add-to-cart-from.tsx
+++ app/(dashboard)/(routes)/shop/forms/add-to-cart-from.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { Minus, Plus } from 'lucide-react';
 import { useState } from 'react';
 
 interface AddToCartFormProps {
@@ -24,24 +25,24 @@ function AddToCartForm({ productId }: AddToCartFormProps) {
       </button>
       <div className="flex">
         <div
-          className="w-12 h-12 flex items-center justify-center bg-green-700 text-whitel"
+          className="w-12 h-12 flex items-center justify-center bg-green-700 text-whitel cursor-pointer"
           onClick={onDecrement}
         >
-          -
+          <Minus />
         </div>
         <span>
           <input
             type="number"
             value={quantity}
             contentEditable
-            className="[appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none]"
+            className="[appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none] w-16 text-2xl text-center pt-2"
           />
         </span>
         <div
-          className="w-12 h-12 flex items-center justify-center bg-green-700 text-whitel"
+          className="w-12 h-12 flex items-center justify-center bg-green-700 text-whitel cursor-pointer"
           onClick={onIncrement}
         >
-          +
+          <Plus />
         </div>
       </div>
     </form>
